5c35b3e4559e6e58b7fcf66639d500dad216e9e1
kush1912 <vidyajaykushwaha@gmail.com>
2022-01-12 17:26:01 +0530

    Order Now button working - This fixes #3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ProductList from '../components/ProductList'
-
+import {Link} from 'react-router-dom'
 import pizza from '../assets/pizza.png'
 
 export default function Home() {
@@ -11,7 +11,9 @@ export default function Home() {
                     <div className='w-1/2 '>
                         <h6 className='text-lg'><em>Are you Hungry?</em></h6>
                         <h1 className='text-3x1 md:text-6xl font-bold'>Don't Wait !</h1>
-                        <button className='py-1 px-4 rounded-full text-white font-bold mt-4 bg-yellow-500 hover:bg-yellow-600'>Order Now</button>
+                        <Link to="/products">
+                            <button className='py-1 px-4 rounded-full text-white font-bold mt-4 bg-yellow-500 hover:bg-yellow-600'>Order Now</button>
+                        </Link>
                     </div>
                     <div className='w-1/2'>
                         <img className='w-4/6' src={pizza} alt='Pizza-icon'></img>
