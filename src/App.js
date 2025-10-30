82249a6c3a60959e2cf1063b13dae032a980a6f7
Ajay Kumar Kushwaha <vidyajaykushwaha@gmail.com>
2025-10-31 03:45:28 +0530

    Fix routing import and add 404 page not found route in App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {BrowserRouter as Router,Routes, Route } from 'react-router-dom' // All are TitleCase
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // All are TitleCase
 //Content inside router needs to be wrapped inside a container tag
 
 import Home from './pages/Home'
@@ -35,6 +35,7 @@ useEffect(()=>{
                         <Route path="/products" exact element={<ProductList/>}></Route>
                         <Route path="/cart" element={<Cart/>}></Route>
                         <Route path="/products/:_id" element={<ProductDetails/>}></Route>
+                        <Route path="*" element={<h1>404 - Page Not Found</h1>} />
                     </Routes>
                 </CartContext.Provider>
             </Router>
